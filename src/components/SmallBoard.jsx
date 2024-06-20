1bf9796a7b255e8a81620e9447ea67543309fdf9
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-06-20 08:20:00 +0530

    Complete Layout of Small Board

--- src/components/SmallBoard.jsx
+++ src/components/SmallBoard.jsx
@@ -1,30 +1,31 @@
 import React, { useState } from "react";
+import styles from "./SmallBoard.module.css"
 
 function SmallBoard() {
-  const initialBoard = Array.fill(null);
+  const initialBoard = Array(9).fill("Q");
   const [board, setBoard] = useState(initialBoard);
   const [currentPlayer, setCurrentPlayer] = useState("X");
 
   const handleClick = (index) => {
-    if (board[index] != null) {
+    if (board[index] !== "Q") {
       return;
     }
 
     const newBoard = board.slice();
     newBoard[index] = currentPlayer;
-    setBoard = newBoard;
+    setBoard(newBoard);
     setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
-
-    return (
-      <div>
-        {board.map((cell, index) => (
-          <button key={index} onClick={() => handleClick(index)}>
-            {cell}
-          </button>
-        ))}
-      </div>
-    );
   };
+
+  return (
+    <div className={styles.board}>
+      {board.map((cell, index) => (
+        <button key={index} onClick={() => handleClick(index)}>
+          {cell}
+        </button>
+      ))}
+    </div>
+  );
 }
 
 export default SmallBoard;
